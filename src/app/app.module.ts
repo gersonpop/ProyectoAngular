432bac13db71ea91e170b60d6241ca9549c3e609
Gerson Porras <Gerson@MacBook-GRP.local>
2020-03-31 17:58:52 -0500

    Creacion de coponentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { RegisterComponent } from './componentes/register/register.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { LoggedComponent } from './componentes/logged/logged.component';
+import { ErrorPageComponent } from './componentes/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent,
+    RegisterComponent,
+    LoginComponent,
+    LoggedComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
