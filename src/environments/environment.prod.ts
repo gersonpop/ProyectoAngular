884459c23a2a585fe5f36ce871ccbd40a1e41fd3
Gerson Porras <Gerson@MacBook-GRP.local>
2020-04-01 10:30:14 -0500

    configuracion firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig = {
+    apiKey: "AIzaSyB2yD_7hwE07LzVErfywazdOXyh_aVcH78",
+    authDomain: "onlinestore-3fc65.firebaseapp.com",
+    databaseURL: "https://onlinestore-3fc65.firebaseio.com",
+    projectId: "onlinestore-3fc65",
+    storageBucket: "onlinestore-3fc65.appspot.com",
+    messagingSenderId: "680860721896",
+    appId: "1:680860721896:web:8ad67654a4a99a623e5f5d",
+    measurementId: "G-C0ETPS1CZN"
+}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig = {
+  apiKey: "AIzaSyB2yD_7hwE07LzVErfywazdOXyh_aVcH78",
+  authDomain: "onlinestore-3fc65.firebaseapp.com",
+  databaseURL: "https://onlinestore-3fc65.firebaseio.com",
+  projectId: "onlinestore-3fc65",
+  storageBucket: "onlinestore-3fc65.appspot.com",
+  messagingSenderId: "680860721896",
+  appId: "1:680860721896:web:8ad67654a4a99a623e5f5d",
+  measurementId: "G-C0ETPS1CZN"
+}
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
