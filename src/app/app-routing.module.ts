212928f9d555c4694c1dc7b56f6aabc2e9aa59f1
Gerson Porras <Gerson@MacBook-GRP.local>
2020-03-31 21:12:53 -0500

    agregar rutas y navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent}from './componentes/home-page/home-page.component'
+import {LoginComponent}from './componentes/login/login.component'
+import {RegisterComponent}from './componentes/register/register.component'
+import {LoggedComponent}from './componentes/logged/logged.component'
+import {ErrorPageComponent}from './componentes/error-page/error-page.component'
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',component: HomePageComponent},
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'logged', component: LoggedComponent},
+  {path:'**',component: ErrorPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
